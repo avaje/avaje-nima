5b3d9a3aadd75ca00625625f5c11bac285b67b9e
Rob Bygrave <robin.bygrave@gmail.com>
2023-09-13 22:54:08 +1200

    small tidy

--- test-avaje-nima/src/main/java/org/example/Main.java
+++ test-avaje-nima/src/main/java/org/example/Main.java
@@ -1,22 +1,10 @@
 package org.example;
 
 import io.avaje.nima.Nima;
-import org.example.api.AddressBuilder;
-import org.example.api.Order;
-import org.example.api.OrderBuilder;
-import org.example.api.OrderLine;
-
-import java.time.LocalDate;
 
 public class Main {
 
-  public static void main(String[] args)  {
-
-    var order = Order.builder()
-      .orderDate(LocalDate.now())
-      .addLines(new OrderLine(23, 10, "asd"))
-      .addLines(new OrderLine(12, 11, "asd"))
-      .build();
+  public static void main(String[] args) {
 
     var webServer = Nima.builder()
       .port(8082)
