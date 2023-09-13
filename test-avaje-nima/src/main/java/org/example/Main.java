20e454fb081f729eaba57ff3335cb33e5729da55
Rob Bygrave <robin.bygrave@gmail.com>
2023-09-14 08:11:31 +1200

    Logback logstash

--- test-avaje-nima/src/main/java/org/example/Main.java
+++ test-avaje-nima/src/main/java/org/example/Main.java
@@ -2,9 +2,15 @@ package org.example;
 
 import io.avaje.nima.Nima;
 
+import java.io.IOException;
+//import java.util.logging.LogManager;
+
 public class Main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+
+//    LogManager.getLogManager().readConfiguration(
+//      Main.class.getResourceAsStream("/logging.properties"));
 
     var webServer = Nima.builder()
       .port(8082)
