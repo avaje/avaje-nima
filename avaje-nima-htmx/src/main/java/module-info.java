939d5b4e50bebce048320fc13ab0ed82f93ed2a4
Rob Bygrave <robin.bygrave@gmail.com>
2025-03-13 08:14:14 +1300

    Add avaje-nima-htmx module (#166)
    This is basically a composite of all the dependencies we need for nima + htmx + jstache
    
    This also moves the DefaultTemplateProvider and JStacheTemplateRender to this module (likely to remove them from avaje-http).

---
+++ avaje-nima-htmx/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module io.avaje.nima.htmx {
+
+  exports io.avaje.htmx.nima.jstache;
+
+  requires transitive io.avaje.htmx.nima;
+  requires transitive io.helidon.webserver;
+  requires transitive io.jstach.jstachio;
+  requires transitive io.avaje.inject;
+  requires static io.avaje.spi;
+
+  provides io.avaje.inject.spi.InjectExtension with io.avaje.htmx.nima.jstache.DefaultTemplateProvider;
+
+}
